2a6a82f93a82d33d6a115a9c251f0fc6c3fdf763
Pasindu Lakshan <lpasindu923@gmail.com>
2026-07-24 09:38:53 +0530

    Revert "Merge pull request #4 from Pasindulakshan2001918/tharindu"
    This reverts commit d8b9aa57195c0983dff55ad6679beb1dbcb33e3a, reversing
    changes made to c244015ee6f295e9747ebbd8520ba38bfe1fefe2.

--- traffic-fine-portal/src/api/fineService.js
+++
@@ -1,9 +0,0 @@
-import api from "./axios";
-
-// GET /api/fines/{referenceNumber}
-export const lookupFine = (referenceNumber) =>
-  api.get(`/fines/${encodeURIComponent(referenceNumber)}`).then((res) => res.data);
-
-// POST /api/fines/{referenceNumber}/pay
-export const payFine = (referenceNumber) =>
-  api.post(`/fines/${encodeURIComponent(referenceNumber)}/pay`).then((res) => res.data);
